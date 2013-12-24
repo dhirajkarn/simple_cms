6f5529083b0454855d258ffa206f51342eba3f3a
dhirajkarn <dhiraj.karn81@gmail.com>
2013-12-24 07:48:32 +0530

    Created subjects form

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -12,4 +12,17 @@ class SubjectsController < ApplicationController
 	def show
 		@subject = Subject.find(params[:id])
 	end
+
+	def new
+		@subject = Subject.new
+	end
+
+	def create
+		@subject = Subject.new(params[:subject])
+		if @subject.save
+			redirect_to(:action => 'list')
+		else
+			render('new')
+		end
+	end
 end
